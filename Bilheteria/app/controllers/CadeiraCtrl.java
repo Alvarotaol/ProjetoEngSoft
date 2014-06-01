015268b9b3956458cb189905c77fb671a2b6883a
Breno Cardoso <elisiobreno@gmail.com>
2014-06-01 14:47:11 -0300

    Coloquei a mudança de Status das Cadeiras, podendo alternar entre disponivel (status = 1) e indisponível (status = 2)

--- Bilheteria/app/controllers/CadeiraCtrl.java
+++ Bilheteria/app/controllers/CadeiraCtrl.java
@@ -6,16 +6,34 @@ import play.data.validation.Required;
 import play.mvc.Controller;
 
 public class CadeiraCtrl extends Controller {
-	public static void criarCadeira(long id_estadio, long id_setor, long id_fileira, String nomeS, String nomeF, @Required String nomeC) {
-            Cadeira c = new Cadeira(nomeC, id_fileira, 1);
-            c.save();
+    public static void criarCadeira(long id_estadio, long id_setor, long id_fileira, String nomeS, String nomeF, @Required String nomeC) {
+        Cadeira c = new Cadeira(nomeC, id_fileira, 1);
+        c.save();
             
-            FileiraCtrl.fileiraExibir(id_estadio, id_setor, id_fileira, nomeS, nomeF);
-	}
+        FileiraCtrl.fileiraExibir(id_estadio, id_setor, id_fileira, nomeS, nomeF);
+    }
     
     public static void cadeiraApagar(long id_estadio, long id_setor, long id_fileira, long id, String nomeS, String nomeF) {
         Cadeira c = Cadeira.find("id", id).first();
         c.delete();
         FileiraCtrl.fileiraExibir(id_estadio, id_setor, id_fileira, nomeS, nomeF);
     }
+    
+    public static void cadeiraTornarDisponivel(long id_estadio, long id_setor, long id_fileira, long id, String nomeS, String nomeF) {
+        Cadeira c = Cadeira.find("id", id).first();
+        
+        c.status = 1;
+        c.save();
+        
+        FileiraCtrl.fileiraExibir(id_estadio, id_setor, id_fileira, nomeS, nomeF);
+    }
+    
+    public static void cadeiraTornarIndisponivel(long id_estadio, long id_setor, long id_fileira, long id, String nomeS, String nomeF) {
+        Cadeira c = Cadeira.find("id", id).first();
+        
+        c.status = 2;
+        c.save();
+        
+        FileiraCtrl.fileiraExibir(id_estadio, id_setor, id_fileira, nomeS, nomeF);
+    }    
 }
