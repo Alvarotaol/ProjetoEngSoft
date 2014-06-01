05ba86408184346462e710942e03b9f1ae3d2370
Breno Cardoso <elisiobreno@gmail.com>
2014-06-01 00:16:11 -0300

    Insere e exclui cadeiras (mas não muda o status)

--- Bilheteria/app/controllers/CadeiraCtrl.java
+++ Bilheteria/app/controllers/CadeiraCtrl.java
@@ -1,7 +1,21 @@
 package controllers;
 
+import models.Cadeira;
+import models.Fileira;
+import play.data.validation.Required;
 import play.mvc.Controller;
 
 public class CadeiraCtrl extends Controller {
+	public static void criarCadeira(long id_estadio, long id_setor, long id_fileira, String nomeS, String nomeF, @Required String nomeC) {
+            Cadeira c = new Cadeira(nomeC, id_fileira, 1);
+            c.save();
+            
+            FileiraCtrl.fileiraExibir(id_estadio, id_setor, id_fileira, nomeS, nomeF);
+	}
     
+    public static void cadeiraApagar(long id_estadio, long id_setor, long id_fileira, long id, String nomeS, String nomeF) {
+        Cadeira c = Cadeira.find("id", id).first();
+        c.delete();
+        FileiraCtrl.fileiraExibir(id_estadio, id_setor, id_fileira, nomeS, nomeF);
+    }
 }
