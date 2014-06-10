a31a1f147c0aeff8a899caeda9cb2409359cd85f
Breno Cardoso <elisiobreno@gmail.com>
2014-06-10 11:39:05 -0300

    Consegue disponibilizar os setores para o jogo

---
+++ Bilheteria/app/controllers/joinSetoresDisponiveis.java
@@ -0,0 +1,43 @@
+package controllers;
+
+
+public class joinSetoresDisponiveis {
+
+    public long getId_setor() {
+        return id_setor;
+    }
+
+    public void setId_setor(long id_setor) {
+        this.id_setor = id_setor;
+    }
+
+    public String getNomeSetor() {
+        return nomeSetor;
+    }
+
+    public void setNomeSetor(String nomeSetor) {
+        this.nomeSetor = nomeSetor;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+    
+    public long id_setor;
+    public String nomeSetor;
+    public int status;
+    public float valor; 
+    
+}
--- Bilheteria/app/models/SetorDisponivelPartida.java
+++ Bilheteria/app/models/SetorDisponivelPartida.java
@@ -6,17 +6,17 @@ import play.db.jpa.Model;
 
 @Entity
 public class SetorDisponivelPartida extends Model{
-	public int status;
-	
-	//chaves estrangeiras
-	public long id_evento;
-	public long id_setor;
-	public float valor;
-	
-	public SetorDisponivelPartida (int status, long id_evento, long id_setor, float valor) {
-                this.status = status;
-		this.id_evento = id_evento;
-		this.id_setor = id_setor;
-		this.valor = valor;
-	}
+    public int status;
+
+    //chaves estrangeiras
+    public long id_evento;
+    public long id_setor;
+    public float valor;
+
+    public SetorDisponivelPartida (long id_evento, long id_setor, int status, float valor) {
+        this.status = status;
+        this.id_evento = id_evento;
+        this.id_setor = id_setor;
+        this.valor = valor;
+    }
 }
